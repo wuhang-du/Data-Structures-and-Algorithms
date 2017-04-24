349f01483744e2d9a1c0dda1017e37d15d396c49
du <duwh@foxmail.com>
2017-04-24 20:17:59 +0800

    551 done

---
+++ 500+/551. Student Attendance Record I.c
@@ -0,0 +1,22 @@
+//
+//date: 2017-4-24
+//desc: 注意审题
+//
+//
+//
+
+
+bool checkRecord(char* s) {
+    int a = 0, l = 0;
+    while(*s != '\0'){
+        while(*s == 'L'){
+            l++;
+            s++;
+        }
+        if( l > 2) return false;
+        l = 0;
+        if(*s++ == 'A') a++;
+    }
+    if(a < 2)  return true;
+    else return false;
+}

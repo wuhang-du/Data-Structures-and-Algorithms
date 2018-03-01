bb0cb32de61cae9a1cc728b11f4815daf20c6c14
duwh <duwh@foxmail.com>
2018-03-01 22:26:09 +0800

    148 add new method

--- 100+/148. Sort List.go
+++ 100+/148. Sort List.go
@@ -33,3 +33,58 @@ func quicksort(start *ListNode, end *ListNode) {
 		}
 	}
 }
+
+func sortList(head *ListNode) *ListNode {
+	return split(head)
+}
+
+func split(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	fast := head
+	slow := head
+
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	fast = slow.Next
+	slow.Next = nil
+
+	// head and fast respent two different list
+	head = split(head)
+	fast = split(fast)
+
+	return merge(head, fast)
+}
+
+func merge(line1 *ListNode, line2 *ListNode) *ListNode {
+
+	var l ListNode
+	cur := &l
+
+	for line1 != nil && line2 != nil {
+		if line1.Val < line2.Val {
+			cur.Next = line1
+			line1 = line1.Next
+		} else {
+			cur.Next = line2
+			line2 = line2.Next
+		}
+		cur = cur.Next
+	}
+
+	if line1 == nil {
+		cur.Next = line2
+	}
+
+	if line2 == nil {
+		cur.Next = line1
+	}
+
+	return l.Next
+}
